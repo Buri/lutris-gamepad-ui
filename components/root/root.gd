4fc8d05273c10102d116d092fdaf0ec73456cbf9
DavidoTek <54072917+DavidoTek@users.noreply.github.com>
2022-01-30 20:38:25 +0100

    Move tab code to root script

--- components/root/root.gd
+++ components/root/root.gd
@@ -1,5 +1,7 @@
 extends Control
 
+onready var tabMain = $VBoxContainer/TabContainer
+
 func _ready():
 	UiStack.create($VBoxContainer/TabContainer)
 
@@ -8,6 +10,20 @@ func _unhandled_input(event):
 		UiStack.close()
 	if event.is_action_pressed("ui_menu"):
 		show_menu_panel()
+	
+	if event.is_action_pressed("ui_tab_next"):
+		if tabMain.current_tab + 1 < tabMain.get_child_count():
+			tabMain.current_tab += 1
+		else:
+			tabMain.current_tab = 0
+	if event.is_action_pressed("ui_tab_prev"):
+		if tabMain.current_tab > 0:
+			tabMain.current_tab -= 1
+		else:
+			tabMain.current_tab = tabMain.get_child_count() - 1
+	var tab_child = tabMain.get_child(tabMain.current_tab)
+	if tab_child.has_method("reset_focus"):
+		tab_child.reset_focus()
 
 func show_menu_panel():
 	var panelScene = preload("res://components/menu_panel/menu_panel.tscn")
