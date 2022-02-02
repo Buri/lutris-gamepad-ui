6de91c01d8c362be9881d4b82a13adb39e2efc66
DavidoTek <54072917+DavidoTek@users.noreply.github.com>
2022-02-02 16:43:30 +0100

    Close menu after selecting option

--- components/menu_panel/menu_panel.gd
+++ components/menu_panel/menu_panel.gd
@@ -1,5 +1,19 @@
 extends Control
 
 
+func _on_ButtonSettings_pressed():
+	# Close menu when opening settings.
+	# Alternatively: show settings "tab" as long as settings button is focused.
+	UiStack.close()
+
+
+func _on_ButtonAbout_pressed():
+	UiStack.close()
+
+
+func _on_ButtonDesktop_pressed():
+	UiStack.close()
+
+
 func _on_ButtonExit_pressed():
 	get_tree().quit()
